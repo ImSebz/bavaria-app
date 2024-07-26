65f4fe36e04dd8b1abe208298fff8c8a43900874
ImSebz <juansecapri@hotmail.com>
2024-07-26 11:44:20 -0500

    Ajustes de mapa

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 import { assets } from '../../assets/assets';
 
-const navbar = () => {
-
+const Navbar = () => {
     const [menu, setMenu] = useState("home");
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === '/') {
+            setMenu("home");
+        } else if (location.pathname === '/mapa') {
+            setMenu("map");
+        }
+    }, [location]);
 
     return (
         <div className='navbar'>
-            <Link to="/" onClick={() => { setMenu("home") }}><img src={assets.logoBaravia} className='logo' alt="Logo" /></Link>
+            <Link to="/" onClick={() => { setMenu("home") }}>
+                <img src={assets.logoBaravia} className='logo' alt="Logo" />
+            </Link>
             <ul className="navbar-menu">
-                <Link to="/" onClick={() => { setMenu("home") }} ><li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Inicio</li></Link>
-                <Link to="/mapa" onClick={() => { setMenu("map") }}><li onClick={() => setMenu("map")} className={menu === "map" ? "active" : ""}>Mapa</li></Link>
+                <Link to="/" onClick={() => { setMenu("home") }}>
+                    <li className={menu === "home" ? "active" : ""}>Inicio</li>
+                </Link>
+                <Link to="/mapa" onClick={() => { setMenu("map") }}>
+                    <li className={menu === "map" ? "active" : ""}>Mapa</li>
+                </Link>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="Search Icon" />
@@ -21,7 +35,7 @@ const navbar = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default navbar;
+export default Navbar;
--- src/pages/map/map.jsx
+++ src/pages/map/map.jsx
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import './map.css';
 
-import { GoogleMap } from '@react-google-maps/api';
+const containerStyle = {
+    width: '100%',
+    height: '800px'
+};
 
-const map = () => {
+const locations = [
+    { name: 'Don Juan', address: 'Av Carlos Escallón Cra 8 # 34-01', lat: 10.4242861308465, lng: -75.5489356306894 },
+    { name: 'The Clock pub', address: 'Calle 34 #7-33 Plaza de los coches', lat: 10.4236940224763, lng: -75.5491482304822 },
+    { name: 'Inka', address: 'Carrera 5 #33-55 Portal de Los Dulces', lat: 10.4233377408453, lng: -75.5497013661753 },
+    { name: 'Casa Cruxada', address: 'Cl. 35 #3-30', lat: 10.4242818986334, lng: -75.5518245146989 },
+    { name: 'Mistura', address: 'Carrera 8 #36-151', lat: 10.426823278194, lng: -75.5484583052338 },
+    { name: 'UMA', address: 'Cll del Curato # 38-99', lat: 10.4280822085015, lng: -75.5484067238443 },
+    { name: 'La Unica', address: 'Cra. 8 #38-47', lat: 10.4275191032595, lng: -75.5479865877157 },
+    { name: 'Hotel Movich', address: 'Calle de Vélez Danies No. 4 – 39', lat: 10.4231751472625, lng: -75.5506840917423 },
+    { name: 'Sophía Rooftop', address: 'Calle 32 #4-45', lat: 10.4226779223445, lng: -75.5504704052338 },
+    { name: 'Marina Todomar', address: 'Carrera 2 #15-364', lat: 10.4178692, lng: -75.5515702 }
+];
 
+function MyComponent() {
     const apiKey = import.meta.env.VITE_API_KEY;
 
-    return (
-        <div>
-            <h1>Mapa</h1>
-        </div>
-    )
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: apiKey
+    });
+
+    const [map, setMap] = useState(null);
+    const [currentPosition, setCurrentPosition] = useState(null);
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setCurrentPosition({ lat: 10.4043241, lng: -75.5522719 });
+                },
+                (error) => {
+                    console.error("Error getting location: ", error);
+                }
+            );
+        }
+    }, []);
+
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null);
+    }, []);
+
+    return isLoaded && currentPosition ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={currentPosition}
+            zoom={15}
+            onUnmount={onUnmount}
+        >
+            {locations.map((location, index) => (
+                <Marker
+                    key={index}
+                    position={{ lat: location.lat, lng: location.lng }}
+                    title={location.name}
+                />
+            ))}
+            <Marker
+                position={currentPosition}
+                icon={{
+                    url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                }}
+            />
+        </GoogleMap>
+    ) : <></>;
 }
 
-export default map
+export default React.memo(MyComponent);
