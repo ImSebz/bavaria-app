3123f8113841c3edf3b0a4a0f723b4f95abcdbbf
Sebastián Camargo <juansecapri@hotmail.com>
2024-12-10 19:22:15 -0500

    Cambios de puntos y avances

--- src/assetsCali/assetsCali.js
+++ src/assetsCali/assetsCali.js
@@ -1,5 +1,17 @@
 import bar from './bar.jpg';
 import cartaIcon from './carta.svg';
+import citadino from './citadino.jpg';
+import citadino1 from './citadino1.jpg';
+import citadino10 from './citadino10.jpg';
+import citadino11 from './citadino11.jpg';
+import citadino2 from './citadino2.jpg';
+import citadino3 from './citadino3.jpg';
+import citadino4 from './citadino4.jpg';
+import citadino5 from './citadino5.jpg';
+import citadino6 from './citadino6.jpg';
+import citadino7 from './citadino7.jpg';
+import citadino8 from './citadino8.jpg';
+import citadino9 from './citadino9.jpg';
 import clubColombia from './club_colombia.svg';
 import clubColombia_logo from './club_colombia_logo.svg';
 import corona from './corona.svg';
@@ -14,9 +26,23 @@ import la_casona1 from './la_casona1.jpeg';
 import la_casona2 from './la_casona2.jpeg';
 import la_casona3 from './la_casona3.jpeg';
 import la_casona4 from './la_casona4.jpg';
+import la_comitiva from './la_comitiva.jpg';
+import la_comitiva1 from './la_comitiva1.jpg';
+import la_comitiva2 from './la_comitiva2.jpg';
+import la_comitiva3 from './la_comitiva3.jpg';
+import la_comitiva4 from './la_comitiva4.jpg';
+import la_comitiva5 from './la_comitiva5.jpg';
 import logoHighEnd from './logo_high_end.svg';
 import mapaIcon from './mapa.svg';
 import marinas from './marinas.jpg';
+import plazoleta_municipal from './plazoleta_municipal.jpg';
+import plazoleta_municipal1 from './plazoleta_municipal1.jpg';
+import plazoleta_municipal2 from './plazoleta_municipal2.jpg';
+import plazoleta_municipal3 from './plazoleta_municipal3.jpg';
+import plazoleta_municipal4 from './plazoleta_municipal4.jpg';
+import plazoleta_municipal5 from './plazoleta_municipal5.jpg';
+import plazoleta_municipal6 from './plazoleta_municipal6.jpg';
+import plazoleta_municipal7 from './plazoleta_municipal7.jpg';
 import reservaIcon from './reserva.svg';
 import restaurante from './restaurante.jpg';
 import rooftop from './rooftop.jpg';
@@ -29,14 +55,6 @@ import tres_fronteras from './tres_fronteras.jpg';
 import tres_fronteras1 from './tres_fronteras1.jpg';
 import x_icon from './x_icon.svg';
 import zur from './zur.jpg';
-import plazoleta_municipal from './plazoleta_municipal.jpg';
-import plazoleta_municipal1 from './plazoleta_municipal1.jpg';
-import plazoleta_municipal2 from './plazoleta_municipal2.jpg';
-import plazoleta_municipal3 from './plazoleta_municipal3.jpg';
-import plazoleta_municipal4 from './plazoleta_municipal4.jpg';
-import plazoleta_municipal5 from './plazoleta_municipal5.jpg';
-import plazoleta_municipal6 from './plazoleta_municipal6.jpg';
-import plazoleta_municipal7 from './plazoleta_municipal7.jpg';
 import zur1 from './zur1.jpg';
 import zur2 from './zur2.jpg';
 import zur3 from './zur3.jpg';
@@ -44,6 +62,18 @@ import zur3 from './zur3.jpg';
 export const assets = {
     bar,
     cartaIcon,
+    citadino,
+    citadino1,
+    citadino10,
+    citadino11,
+    citadino2,
+    citadino3,
+    citadino4,
+    citadino5,
+    citadino6,
+    citadino7,
+    citadino8,
+    citadino9,
     clubColombia_logo,
     clubColombia,
     corona_logo,
@@ -58,13 +88,15 @@ export const assets = {
     la_casona2,
     la_casona3,
     la_casona4,
+    la_comitiva,
+    la_comitiva1,
+    la_comitiva2,
+    la_comitiva3,
+    la_comitiva4,
+    la_comitiva5,
     logoHighEnd,
     mapaIcon,
     marinas,
-    reservaIcon,
-    restaurante,
-    rooftop,
-    stella_logo,
     plazoleta_municipal,
     plazoleta_municipal1,
     plazoleta_municipal2,
@@ -73,6 +105,10 @@ export const assets = {
     plazoleta_municipal5,
     plazoleta_municipal6,
     plazoleta_municipal7,
+    reservaIcon,
+    restaurante,
+    rooftop,
+    stella_logo,
     stella,
     tizones,
     tizones1,
@@ -94,6 +130,10 @@ export const sites_list_cali = [
     {
         site_name: "Rooftop",
         site_image: rooftop
+    },
+    {
+        site_name: "Clubes",
+        site_image: restaurante
     },
 
     {
@@ -250,6 +290,52 @@ export const location_list_cali = [
         subtitle: "Restaurante",
         url_reserva: "https://linktr.ee/plazuelamunicipalco?fbclid=PAZXh0bgNhZW0CMTEAAaZrQbcOY734oSAOTx4JojElGC94bBOcG0-_quYamRM25FaC6CQ8RdhtVAc_aem_tBalrhWr-AUDX1i3AFI3nA"
     
+    }, 
+    {
+        _id: "7",
+        address: "CRA 2 OESTE 2 21",
+        carta: "https://app.menupp.co/restaurant/citadinogastrobar?fbclid=PAZXh0bgNhZW0CMTEAAaa1TntminFdVTUf1qIPO4XOojJ2oohAjWdpxvT2HGMh1MQ0ZuCrGUac7Bg_aem_v4F0kCnQkWcrB-Yan-OcLw",
+        category: "Restaurante",
+        description: "",
+        dress_code: "Camisa blanca, delantal negro", 
+        image_carousel: [citadino1, citadino2, citadino3, citadino4, citadino5, citadino6, citadino7, citadino8, citadino9, citadino10, citadino11],
+        image: citadino,
+        lat: 3.4500108,
+        lng: -76.5432875,
+        marca_logo: clubColombia_logo,
+        marca: clubColombia,
+        marker: null,
+        name: "Citadino Gastro Bar",
+        promo: "",
+        schedule: "12:00m - 1:00am",
+        subtitle: "Restaurante",
+        url_reserva: "https://app.menupp.co/restaurant/citadinogastrobar?fbclid=PAZXh0bgNhZW0CMTEAAaa1TntminFdVTUf1qIPO4XOojJ2oohAjWdpxvT2HGMh1MQ0ZuCrGUac7Bg_aem_v4F0kCnQkWcrB-Yan-OcLw"
+    },
+    {
+        _id: "8",
+        address: "CL 4 34-32",
+        carta: "https://idmenu.club/lacomitiva/?fbclid=PAZXh0bgNhZW0CMTEAAaa_ThxZBZUcc9Tohg2gZF5CEYO7ZgoZM0E6eQl4ecl8V5Fe_xmlYaS4q8I_aem_K0uA6GQT4053vvPPrrdjSA",
+        category: "Restaurante",
+        description: "",
+        dress_code: "Camisa negra manga larga, pantalon negro",
+        image_carousel: [la_comitiva1, la_comitiva2, la_comitiva3, la_comitiva4, la_comitiva5],
+        image: la_comitiva,
+        lat: 3.4343135,
+        lng: -76.5452552,
+        marca_logo: clubColombia_logo,
+        marca: clubColombia,
+        marker: null,
+        name: "La Comitiva",
+        promo: "",
+        schedule: "12:00m - 1:00am",
+        subtitle: "Restaurante",
+        url_reserva: "https://idmenu.club/lacomitiva/?fbclid=PAZXh0bgNhZW0CMTEAAaa_ThxZBZUcc9Tohg2gZF5CEYO7ZgoZM0E6eQl4ecl8V5Fe_xmlYaS4q8I_aem_K0uA6GQT4053vvPPrrdjSA"
+    },
+    {
+        _id: "9",
+        address: "AV 9N 17A-36",
+        
     }
+
 
 ]
